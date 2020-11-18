cc925e1f6c95f53ae2fec990316c67ee2ac6c92a
OSCAR IGNACIO MIRANDA OLMOS <oi.mirandaolmos@ugto.mx>
2020-11-18 13:26:38 -0600

    Cambios on FWEB por OscarMiranda 1:26pm

--- PWFractal/js/aboutus.js
+++ PWFractal/js/aboutus.js
@@ -114,3 +114,37 @@ particlesJS(
     "retina_detect": true
   }
 );
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(event) { 
+
+  var slides = document.querySelectorAll("#slides .slide");
+  var currentSlide = 0;
+  
+  //Uncomment for auto play
+  // var slideInterval = setInterval(nextSlide,2000);
+    
+  function nextSlide(){
+    slides[currentSlide].classList.remove("showing");
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].classList.add("showing");
+    console.log("next slide");
+  }
+  
+  function prevSlide(){
+    if(currentSlide > 0){
+    slides[currentSlide].classList.remove("showing");
+    currentSlide = (currentSlide-1)%slides.length;
+    slides[currentSlide].classList.add("showing");
+    console.log("prev slide");
+    }
+    console.log("no previous slide")
+  }
+  
+   
+    
+  // });
+  
