dd2c6bb4ec1e7d372d1bd411c5f6c21a3ce306a7
OSCAR IGNACIO MIRANDA OLMOS <oi.mirandaolmos@ugto.mx>
2021-01-13 09:03:47 -0600

    BlogInitial Grid F1 OscarMiranda 13/01/2021

--- PWFractal/js/js.js
+++ PWFractal/js/js.js
@@ -2,3 +2,21 @@ window.onload = function(){
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
   }
+
+
+
+
+  $(function() {
+  
+    $(document).on('scroll resize', function() {
+      
+      var $d = $(document),
+          $w = $(window);
+      
+      $('div#scroll-bar').width(
+        ($d.scrollTop() / ($d.height() - $w.height()) * $d.height()) + 'px'
+      );
+      
+    });
+   
+ });
