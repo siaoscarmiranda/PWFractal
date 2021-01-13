f2e1984151e26aea420e5e4855f46573e3a1a4cc
OSCAR IGNACIO MIRANDA OLMOS <oi.mirandaolmos@ugto.mx>
2021-01-13 13:12:29 -0600

    NuevosCambios OscarMiranda

--- PWFractal/js/js.js
+++ PWFractal/js/js.js
@@ -6,17 +6,13 @@ window.onload = function(){
 
 
 
-  $(function() {
+  function progressBarScroll() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+  }
   
-    $(document).on('scroll resize', function() {
-      
-      var $d = $(document),
-          $w = $(window);
-      
-      $('div#scroll-bar').width(
-        ($d.scrollTop() / ($d.height() - $w.height()) * $d.height()) + 'px'
-      );
-      
-    });
-   
- });
+  window.onscroll = function () {
+    progressBarScroll();
+  };
